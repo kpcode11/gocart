995dcb113f08478857424656ed1b2c74292c7bf5
kpcode11 <prajapatikeshav497@gmail.com>
2026-01-18 13:35:02 +0530

    fix: Package path ./dist/types/server for getAuth

--- app/api/admin/dashboard/route.js
+++ app/api/admin/dashboard/route.js
@@ -1,6 +1,6 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
-import { auth, getAuth } from "@clerk/nextjs/dist/types/server";
+import { getAuth } from "@clerk/nextjs/server";
 import authAdmin from "@/middlewares/authAdmin";
 
 export async function GET(request) {
--- app/api/admin/is-admin/route.js
+++ app/api/admin/is-admin/route.js
@@ -1,5 +1,5 @@
 import authAdmin from "@/middlewares/authAdmin";
-import { getAuth } from "@clerk/nextjs/dist/types/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
